bc4d818eda211b0ec946da0f07580669f80c72f4
Генец Евгений <genets@crm-expert.biz>
2021-01-10 17:58:18 +0500

    рефакторинг

--- src/com/company/ThreeFatMan.java
+++ src/com/company/ThreeFatMan.java
@@ -1,30 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.company;
 
 import java.util.*;
 import java.io.*;
 
-/**
- *
- * @author Евгений
- */
-// Вопросы:
-//1. Для чего такая конструкция, а не просто Scanner(new File("test.txt")) ?
-//      FileReader reader = new FileReader(new File("D:\temp\test.txt"));
-//      Scanner scan = new Scanner(reader);
-//      или
-//      FileInputStream fis = new FileInputStream(input);
-//
-//      Scanner scan = new Scanner(fis);
-//2. Не получается использовать эту строку без try
-// Scanner scanner = new Scanner(new File("massa.txt"))
 public class ThreeFatMan {
-
-
     static int findGreatestMassa(int[] massa) {
         int i = 1;
 
